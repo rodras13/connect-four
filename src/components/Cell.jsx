ea7e68540c27459fc99de4a2a5bd3185d784303d
rodras13 <koscinskirodrigo@gmail.com>
2024-11-26 19:34:33 +0100

    Add numbers to improve undestanding

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,7 +1,13 @@
-function Cell() {
+import PropTypes from "prop-types";
+
+function Cell({ children }) {
   return (
-    <div className="bg-violet-200 size-14 rounded-full"></div>
+    <div className="bg-violet-200 size-14 rounded-full flex justify-center items-center text-black">{children}</div>
   );
 }
+
+Cell.propTypes = {
+  children: PropTypes.number
+};
 
 export default Cell;
